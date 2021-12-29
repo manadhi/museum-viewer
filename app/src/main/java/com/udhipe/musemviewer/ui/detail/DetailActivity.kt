5ac8d21cf0d8ef9479195bfdbcd3a649ba9bfe94
udhiGorry <mac@Macs-MacBook-Pro.local>
2021-12-29 19:13:37 +0700

    feature:initial commit

---
+++ app/src/main/java/com/udhipe/musemviewer/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.udhipe.musemviewer.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.udhipe.musemviewer.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/com/udhipe/musemviewer/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.udhipe.musemviewer.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.udhipe.musemviewer.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
